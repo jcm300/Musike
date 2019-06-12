851db489a79d25696412538ac4c82b49714f87f8
jcm300 <zeclmartins@gmail.com>
2019-06-13 00:38:49 +0100

    add graphdb 1º part

--- src/install.sh
+++ src/install.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 1 ]; then
-    if [ $1 = "p" ]; then
-        #Clone rep
-        git clone https://github.com/jcm300/Musike.git
-
-        echo "Change the private and public key in the folder auth, change the port in bin/www. When done press some letter to continue."
-        read -n 1 -p "Continue?";
-
-        cd Musike/src
-        mkdir mongoDB
-        #start mongoDB
-        mongod --dbpath ./mongoDB/ &
-
-        #start graphDB in server mode
-        graphdb-free-8.9.0/bin/graphdb -s &
-
-        cd APIserver
-        npm install
-        npm start "127.0.0.1:27017" "localhost:7200" &
-
-        cd ../interface
-        npm install -g @vue/cli
-        npm install
-        npm run serve
-    elif [ $1 = "i" ]; then
+    if [ $1 = "i" ]; then
         mkdir mongoDB
         #start mongoDB
         mongod --dbpath ./mongoDB/ &
@@ -59,13 +36,11 @@ if [ $# -eq 1 ]; then
     elif []; then
         echo "Wrong Parameter"
         echo "Please insert a paramenter:"
-        echo "p --> Production"
         echo "i --> Install"
         echo "r --> Run"
     fi
 elif [ 1 ]; then
     echo "Please insert a paramenter:"
-    echo "p --> Production"
     echo "i --> Install"
     echo "r --> Run"
 fi
